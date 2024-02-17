9c6e266a1c85547c54b5bec94317186b904ce9ba
Kiselt <157617885+Kiselt@users.noreply.github.com>
2024-02-17 21:24:48 +0100

    added function for clear button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,49 +1,55 @@
 const gridContainer = document.querySelector("#grid-container");
 const resolutionBtn = document.querySelector("#resolution");
 const randomColorBtn = document.querySelector("#random-color");
+const clearBtn = document.querySelector("#clear-sketch");
 
 const createGrid = (resolution) => {
   let tilesQuantity = resolution ** 2;
-  const sketchapadSize = 960;
+  const sketchapadSize = 800;
   const calcTileSize = sketchapadSize / resolution;
   const tileSize = calcTileSize + "px";
 
   for (let tile = 0; tile < tilesQuantity; tile++) {
-      const gridTile = gridContainer.appendChild(document.createElement("div"));
+      const gridTile = document.createElement("div");
       gridTile.setAttribute("class", "grid-tile");
       gridTile.style.width = tileSize;
       gridTile.style.height = tileSize;
+
+      gridTile.addEventListener("mouseover", () => {
+      gridTile.style.backgroundColor = "black";
+        });
+
+        randomColorBtn.addEventListener("click", () => {
+          gridTile.addEventListener("mouseover", () => {
+            gridTile.style.backgroundColor = randomRgbColor()
+            //console.log(randomRgbColor())
+          });
+        });
+        gridContainer.appendChild(gridTile);
+      }
     }
-    const tilesArray = Array.from(gridContainer.childNodes);
-    tilesArray.forEach(element => {
-    element.addEventListener("mouseover", () => {
-    element.style.backgroundColor = "black"
-      });
-    });
 
     function randomInteger(max) {
       return Math.floor(Math.random()*(max + 1));
-    }
-
-    function randomNumber() {
-      return Math.random().toFixed(2);
     }
   
     function randomRgbColor() {
       let r = randomInteger(255);
       let g = randomInteger(255);
       let b = randomInteger(255);
-      let a = randomNumber(1);
-      return `rgba(${r}, ${g}, ${b}, ${a})`;
+      return `rgb(${r}, ${g}, ${b})`;
     }
-    
-    randomColorBtn.addEventListener("click", () => {
-      tilesArray.forEach(element => {
-      element.addEventListener("mouseover", () => {
-      element.style.backgroundColor = randomRgbColor()
-      console.log(randomRgbColor())
+
+    function clearSketchpad() {
+      let gridTile = document.getElementById("grid-container").childNodes;
+      gridTile.forEach(element => {
+        element.style.backgroundColor = "white";
       });
-    });
+    }
+
+  // Immediately create the deafult grid on load
+  document.addEventListener("DOMContentLoaded", () => {
+    createGrid(16);
   });
 
 //Function for grid resolution change
@@ -54,5 +60,5 @@ resolutionBtn.addEventListener("click", () => {
     const resolution = prompt("Choose desired sketchpad resolution (max size = 100)");
     createGrid(resolution);
 });
-}
-createGrid(16);
+
+clearBtn.addEventListener("click", clearSketchpad);
